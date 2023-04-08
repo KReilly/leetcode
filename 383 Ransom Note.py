130e40e62aa12db6b90de6242111aba6322be758
kreilly <Kreilly@gmail.com>
2023-04-08 12:04:16 -0500

    Create 383 Ransom Note.py

---
+++ 383 Ransom Note.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dict1 = dict(Counter(ransomNote))
+        dict2 = dict(Counter(magazine))
+        
+        for i in dict1 :
+            if not i in dict2 :
+                return False
+            else:
+                if dict1[i] > dict2[i] :
+                    return False
+        return True
+
+
+ransomNote = "a"
+magazine = "b"
+ransomNote = "aa"
+magazine = "ab"
+ransomNote = "aa"
+magazine = "aab"
+
+print(Solution.canConstruct(Solution, ransomNote, magazine))
