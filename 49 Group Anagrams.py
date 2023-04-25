61455d1ebcfadd5793a69d03030ff4d8c3eabe18
kreilly <Kreilly@gmail.com>
2023-04-25 13:29:29 -0500

    Update 49 Group Anagrams.py

--- 49 Group Anagrams.py
+++ 49 Group Anagrams.py
@@ -3,19 +3,19 @@ import collections
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
-        # d = collections.defaultdict(list)
+        # ans = collections.defaultdict(list)
         # for s in strs:
-        #     s2 = ''.join(sorted(s))
-        #     d[s2] = d[s2].append(s)
-        # ans = []
-        # for l in d:
-        #     ans.append(l)
+        #     ans[tuple(sorted(s))].append(s)
+        # return ans.values()
+        d = collections.defaultdict(list)
+        for s in strs:
+            s2 = ''.join(sorted(s))
+            d[tuple(sorted(s))].append(s)
+        ans = []
+        for l in d:
+            ans.append(d[l])
 
-        # return ans
+        return ans
     
 strs = ["eat","tea","tan","ate","nat","bat"]
 # strs = [""]
