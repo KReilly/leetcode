a4f7e25f3d9bd7628033cdeb76eb522f73791fca
kreilly <Kreilly@gmail.com>
2023-05-26 08:40:56 -0500

    Create 1323 Maximum 69 Number.py

---
+++ 1323 Maximum 69 Number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = str(num)
+        r = ""
+        hasChanged = False
+        for c in s:
+            if( hasChanged == False and c == "6"):
+                r += "9"
+                hasChanged = True
+            else:
+                r += c
+        print(r)
+
+        return int(r)
+
+
+num = 9669
+print(Solution.maximum69Number(Solution, num))
