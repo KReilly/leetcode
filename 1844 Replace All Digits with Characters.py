61e04146812ff90658dd5884dda66b20c072a9ab
kreilly <Kreilly@gmail.com>
2023-06-15 15:23:24 -0500

    Update 1844 Replace All Digits with Characters.py

--- 1844 Replace All Digits with Characters.py
+++ 1844 Replace All Digits with Characters.py
@@ -1,15 +1,19 @@
 class Solution:
     def replaceDigits(self, s: str) -> str:
-        n = ""
+        # n = ""
 
-        for i in range(len(s)//2):
-            n += s[i*2]
-            a = chr(ord(s[i*2])+int(s[i*2+1]))
-            print(a)
-            n+=a
+        # for i in range(len(s)//2):
+        #     n += s[i*2]
+        #     if()
+        #     a = chr(ord(s[i*2])+int(s[i*2+1]))
+        #     n+=a
 
 
-        return n
+        # return n
+        a = list(s)
+        for i in range(1, len(a), 2):
+            a[i] = chr(ord(a[i - 1]) + int(a[i]))
+        return ''.join(a)
 
 
 s = "a1c1e1"
