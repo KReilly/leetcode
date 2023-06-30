56f9138ec64ec79f3fee606996d2b63250a9bcb8
kreilly <Kreilly@gmail.com>
2023-06-30 10:58:05 -0500

    Update 2520 Count the Digits That Divide a Number.py

--- 2520 Count the Digits That Divide a Number.py
+++ 2520 Count the Digits That Divide a Number.py
@@ -1,2 +1,14 @@
 class Solution:
     def countDigits(self, num: int) -> int:
+        n = num
+        count = 0
+        while(n > 0):
+            d = n%10
+            print(d)
+            if(num%d == 0):
+                count +=1
+            n = n//10
+        return count
+
+num = 7
+print(Solution.countDigits(Solution, num))
