449ceb06e9404ebab9a15378f7d7ca7fdbbcd6ca
kreilly <Kreilly@gmail.com>
2023-07-24 11:52:48 -0500

    Create 2769 Find the Maximum Achievable Number.py

---
+++ 2769 Find the Maximum Achievable Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2*t
+    
+num = 4
+t = 1
+
+print(Solution.theMaximumAchievableX(Solution, num, t))
