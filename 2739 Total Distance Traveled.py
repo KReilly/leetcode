deabbf8d2849d5e5df75a679f631708b548c32ec
kreilly <Kreilly@gmail.com>
2023-07-26 11:46:20 -0500

    Update 2739 Total Distance Traveled.py

---
+++ 2739 Total Distance Traveled.py
@@ -0,0 +1,20 @@
+class Solution:
+    def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+        result = 0
+        i = 0
+        while i < mainTank:
+            i += 1
+            if (additionalTank != 0) and (i % 5 == 0):
+                additionalTank -= 1
+                result += 10
+                mainTank += 1
+            else:
+                result += 10
+        return result
+
+
+
+mainTank = 5
+additionalTank = 10
+
+print(Solution.distanceTraveled(Solution, mainTank, additionalTank))
