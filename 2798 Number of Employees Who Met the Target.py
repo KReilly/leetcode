3c54f2ea6b2bcc4f9a578ed0620372ebdbfe0734
kreilly <Kreilly@gmail.com>
2023-07-31 10:31:01 -0500

    Create 2798 Number of Employees Who Met the Target.py

---
+++ 2798 Number of Employees Who Met the Target.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        c = 0
+        for h in hours:
+            if(h>=target):
+                c+=1
+        return c
