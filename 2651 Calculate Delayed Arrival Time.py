87fae756f13416afda5adbfdc7477af97613664d
kreilly <Kreilly@gmail.com>
2023-08-02 16:20:25 -0500

    Update 2651 Calculate Delayed Arrival Time.py

--- 2651 Calculate Delayed Arrival Time.py
+++ 2651 Calculate Delayed Arrival Time.py
@@ -2,4 +2,6 @@ class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
         return (arrivalTime+delayedTime)%24
 
-
+arrivalTime = 10
+delayedTime = 15
+print(Solution.findDelayedArrivalTime(Solution, arrivalTime, delayedTime))
