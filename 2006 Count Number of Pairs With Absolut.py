8bfedb300a328b872820ec9b8029962a0bd8b7b8
kreilly <Kreilly@gmail.com>
2023-08-09 17:08:29 -0500

    Create 2006 Count Number of Pairs With Absolut.py

---
+++ 2006 Count Number of Pairs With Absolut.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        c = 0
+
+
+
+        return c
+    
+
+nums = [1,2,2,1]
+
+k = 1
+
+print(Solution.countKDifference(Solution, nums, k))
